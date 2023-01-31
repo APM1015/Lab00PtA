17f390b27e0269dcdc3844fe2bde0fb843bb3283
APM1015 <122992723+APM1015@users.noreply.github.com>
2023-01-31 18:28:08 -0500

    Create Tulsa.c

---
+++ Tulsa.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <string.h>//to use strcat to append the path
+
+
+void tuls(const char *string);
+
+int main(int argc, char const *argv[]) {
+    //Prompts error if invalid number of arguments
+    if(argc < 0){
+        perror("Invalid number of arguments. Program failed.");
+        exit(EXIT_FAILURE);
+    }
+    //No recursion since 0 arguments
+    //Lists all directory
+    if(argc == 0 || argc >= 1){
+        tuls(".");
+    }
+
+    return 0;
+}
+
+void tuls(const char *dir) {
+    //list the directoru
+    struct dirent *d;
+    DIR *dh = opendir(dir);
+    if(!dh){
+        //if(errno = ENOENT){
+        //   perror("Directory entered does not exist");
+        //}
+        // else{
+        perror("tuls: cannot open directory\n");
+        //}
+        exit(EXIT_FAILURE);
+    }
+    // while( (direntStructPtr = readdir(DIRPtr)) != NULL){}
+    while((d = readdir(dh)) != NULL){
+        //skips over
+        if(d->d_name[0] == '.' || d->d_name[0] == '..' ) {
+            continue;
+        }
+       printf("%s ", d->d_name);
+       if((d = readdir(dh)) == DT_DIR){
+           //array size recomended from discord
+           char path[4096] = {0};
+           strcat(path, dir);
+           strcat(path,"/");
+           strcat(path, (d=readdir(dh)->d_name));
+           tuls(path);
+       }
+
+      // d = readdir(dh);
+
+    }
+    closedir(dh);
+}

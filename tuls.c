69ee9360b4b2f103711ece9eda0e0b6c41b26f69
APM1015 <122992723+APM1015@users.noreply.github.com>
2023-01-31 22:35:54 -0500

    Update tuls.c

--- tuls.c
+++ tuls.c
@@ -9,13 +9,17 @@ void tuls(const char *string);
 
 int main(int argc, char const *argv[]) {
     //Prompts error if invalid number of arguments
-   
+    /*if(argc < 0){
+        perror("Invalid number of arguments. Program failed.");
+        exit(EXIT_FAILURE);
+    }
     //No recursion since 0 arguments
     //Lists all directory
-    
+    if(argc == 0 || argc >= 1){
         tuls(".");
-   
-
+    }
+     */
+    tuls(".");
     return 0;
 }
 
@@ -24,18 +28,29 @@ void tuls(const char *dir) {
     struct dirent *d;
     DIR *dh = opendir(dir);
     if(!dh){
+
         perror("tuls: cannot open directory\n");
+
         exit(EXIT_FAILURE);
     }
     // while( (direntStructPtr = readdir(DIRPtr)) != NULL){}
     d = readdir(dh);
     while(d != NULL){
         //skips over
-        
-       printf("%s ", d->d_name);
-        d = readdir(dh);
-       
+        if(d->d_name == '.' || d->d_name == "..") {
+            continue;
+        }
+       printf("%s \n", d->d_name);
+       if((d = readdir(dh)) == DT_DIR){
+           //array size recomended from discord
+           char path[4096] = {0};
+           strcat(path, dir);
+           strcat(path,"/");
+           strcat(path, (d=readdir(dh)->d_name));
+           tuls(path);
+       }
 
+      // d = readdir(dh);
 
     }
     closedir(dh);

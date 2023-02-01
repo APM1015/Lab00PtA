a2bd380b9730238030031b5c4fbf97ab805c7ec8
APM1015 <122992723+APM1015@users.noreply.github.com>
2023-02-01 01:28:27 -0500

    Update tuls.c

--- tuls.c
+++ tuls.c
@@ -6,6 +6,7 @@
 
 
 void tuls(const char *string);
+//void recursiveDir(const char *string);
 
 int main(int argc, char const *argv[]) {
     //Prompts error if invalid number of arguments
@@ -13,17 +14,66 @@ int main(int argc, char const *argv[]) {
         perror("Invalid number of arguments. Program failed.");
         exit(EXIT_FAILURE);
     }
+    else if(argc > 2){
+        perror("Invalid number of arguments. Program failed.");
+        exit(EXIT_FAILURE);
+    }
     //No recursion since 0 arguments
     //Lists all directory
-   //if(argc == 0 || argc >= 1){
-    //    tuls(".");
-   // }
-    tuls(".");
-    tuls("..");
+   else if(argc == 0 || argc >= 1){
+        tuls(".");
+   }
+   //else if(argc == 2){
+      // tuls(".");
+      // recursiveDir(argv[1]);
+
+  // }
+
+    //tuls(".");
+    //tuls("..");
 
 
     return 0;
 }
+
+//void recursiveDir(const char *string) {
+//list the directoru
+/*
+    struct dirent *d;
+    DIR *dh = opendir(dir);
+    if(!dh){
+
+    perror("tuls: cannot open directory\n");
+
+    exit(EXIT_FAILURE);
+    }
+    // while( (direntStructPtr = readdir(DIRPtr)) != NULL){}
+    d = readdir(dh);
+    while(d != NULL){
+    //skips over
+    //if(d->d_name[0] == '.' || d->d_name[0] == "..") {
+    //continue;
+    //}
+    if((strcmp(d->d_name, ".") == 0) && (strcmp(d->d_name, "..") == 0)){
+    continue;
+    }
+    printf("%s \n", d->d_name);
+
+    if((d = readdir(dh)) == DT_DIR){
+    //array size recomended from discord
+    char path[4096] = {0};
+    strcat(path, dir);
+    strcat(path,"/");
+    strcat(path, (d=readdir(dh)->d_name));
+    tuls(path);
+    }
+
+    // d = readdir(dh);
+
+    }
+    closedir(dh);
+    */
+//}
 
 void tuls(const char *dir) {
     //list the directoru
@@ -39,10 +89,14 @@ void tuls(const char *dir) {
     d = readdir(dh);
     while(d != NULL){
         //skips over
-        if(d->d_name == '.' || d->d_name == "..") {
+        //if(d->d_name[0] == '.' || d->d_name[0] == "..") {
+            //continue;
+        //}
+        if((strcmp(d->d_name, ".") == 0) && (strcmp(d->d_name, "..") == 0)){
             continue;
         }
        printf("%s \n", d->d_name);
+
        if((d = readdir(dh)) == DT_DIR){
            //array size recomended from discord
            char path[4096] = {0};
